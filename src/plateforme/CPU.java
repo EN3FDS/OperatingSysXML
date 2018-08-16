d19425407715946ecf29b351a0eb77dea6877ccb
stannlag <stannlag.outlook.com>
2018-08-15 22:38:18 -0400

    Debut Remaniement

--- src/plateforme/CPU.java
+++ src/plateforme/CPU.java
@@ -56,15 +56,8 @@ public class CPU extends Thread{
 			flagsRegister = rand.nextInt();
 		}
 
-		public synchronized void execute(Process p, PCB pcb) {//aura une boucle for qui partira de l'adresse actuelle a l'adresse finale du process. si gon interrupt lap fe yon break
-			//epitou, nap fe IP a pran valeur i+1 ki reprezante next adress d'execution. 
-			//le gen interrupt, lap rele main nan kap rele scheduler a kap regle kose li byen pwop
-			// Execute and return the interrupt number
-			/*
-			 * cette methode devra recevoir et le pcb et le process
-			 * le pcb pour savoir l'adresse actuelle permettant de commencer l'execution 
-			 * process pour effectivement faire cette execution
-			 */
+		//cette methode est utilisee par l'executeur 
+		public synchronized void execute(Process p, PCB pcb) {
 			byte i;
 			for (i=pcb.getAddressIP();i<=pcb.getFinalAddress();i++) {
 				IP= i+1;
